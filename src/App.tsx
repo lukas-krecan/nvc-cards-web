38ec5733059912ed9160b66377888e46f8c10998
Lukas Krecan <lukas@krecan.net>
2023-03-12 20:54:47 +0100

    First version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Container} from "react-bootstrap";
+import {CardInfo, CardData, feelings, findCard, needs} from './Data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="row text-center text-lg-start">
+                    {needs.map(c => {
+                        return <div className="card col-lg-3 col-md-4 col-sm-12">
+                            <div className="card-body">
+                                {c.data.map(t => <p className="card-text">{t.text}</p>)}
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;

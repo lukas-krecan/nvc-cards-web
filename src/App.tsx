2297fca3cdea98a5a5a1a9208f8bd364e721cd11
Lukas Krecan <lukas@krecan.net>
2023-03-12 21:11:30 +0100

    Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, {memo} from 'react';
 import './App.css';
-import {CardData, needs} from './Data';
+import {CardData, CardInfo, feelings, needs} from './Data';
+import {Tab, Tabs} from "react-bootstrap";
 
 function App() {
-    function fontClass(t: CardData) {
-        return "text" + (t.size || 1);
-    }
-
     return (
         <div className="App">
-            <div className="container">
-                <div className="row text-center text-lg-start">
-                    {needs.map(c => {
-                        return <div className="card col-lg-3 col-md-4 col-sm-12">
-                            <div className="card-body">
-                                {c.data.map(t => <p className={fontClass(t)}>{t.text}</p>)}
-                            </div>
-                        </div>
-                    })}
-                </div>
-            </div>
+            <Tabs>
+                <Tab eventKey="needs" title="Potřeby">
+                    <CardList cards={needs} selectedCards={[]} onCardClick={c => null} />
+                </Tab>
+                <Tab eventKey="feelings" title="Pocity">
+                    <CardList cards={feelings} selectedCards={[]} onCardClick={c => null} />
+                </Tab>
+            </Tabs>
         </div>
     );
 }
+
+type CardListProps = {
+    cards: CardInfo[];
+    selectedCards: string[];
+    onCardClick: (card: CardInfo) => void;
+};
+
+const CardList = (props: CardListProps) => {
+    const {cards} = props;
+
+    const isSelected = (item: CardInfo): boolean => {
+        return props.selectedCards.indexOf(item.id) !== -1;
+    };
+
+    const key = (card: CardInfo) => card.id;
+
+    const fontClass = (t: CardData) => "text" + (t.size || 1);
+
+    return (
+        <div className="container">
+            <div className="row text-center text-lg-start">
+                {cards.map(c => {
+                    return <div className="card col-lg-3 col-md-4 col-sm-12">
+                        <div className="card-body">
+                            {c.data.map(t => <p className={fontClass(t)}>{t.text}</p>)}
+                        </div>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+};
 
 export default App;

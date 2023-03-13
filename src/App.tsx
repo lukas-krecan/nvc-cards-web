70ee48e0a9a8880c62e46cd09b36aa260c7379e2
Lukas Krecan <lukas@krecan.net>
2023-03-13 08:31:20 +0100

    Selection

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,46 @@ import './App.css';
 import {CardData, CardInfo, feelings, needs} from './Data';
 import {Tab, Tabs} from "react-bootstrap";
 
-function App() {
-    return (
-        <div className="App">
-            <Tabs>
-                <Tab eventKey="needs" title="Potřeby">
-                    <CardList cards={needs} selectedCards={[]} onCardClick={c => null} />
-                </Tab>
-                <Tab eventKey="feelings" title="Pocity">
-                    <CardList cards={feelings} selectedCards={[]} onCardClick={c => null} />
-                </Tab>
-            </Tabs>
-        </div>
-    );
+type NvcCardsAppProps = {};
+
+type NvcCardsAppState = {
+    selectedCards: string[];
+};
+
+class App extends React.Component<
+    NvcCardsAppProps,
+    NvcCardsAppState
+> {
+    constructor(props: NvcCardsAppProps) {
+        super(props);
+        this.state = {
+            selectedCards: [],
+        };
+    }
+
+    private selectCard(item: CardInfo) {
+        let selected = this.state.selectedCards;
+        if (selected.indexOf(item.id) !== -1) {
+            this.setState({selectedCards: selected.filter((id) => id !== item.id)});
+        } else {
+            this.setState({selectedCards: selected.concat([item.id])});
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Tabs>
+                    <Tab eventKey="needs" title="Potřeby" key="needs">
+                        <CardList cards={needs} selectedCards={this.state.selectedCards} onCardClick={this.selectCard.bind(this)}/>
+                    </Tab>
+                    <Tab eventKey="feelings" title="Pocity" key="feelings">
+                        <CardList cards={feelings} selectedCards={this.state.selectedCards} onCardClick={this.selectCard.bind(this)}/>
+                    </Tab>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
 type CardListProps = {
@@ -35,13 +62,28 @@ const CardList = (props: CardListProps) => {
 
     const fontClass = (t: CardData) => "text" + (t.size || 1);
 
+
+    function selectedClass(card: CardInfo) {
+        if (!isSelected(card)) {
+            return "";
+        } else {
+            return card.id.startsWith('n')
+                ? "selectedNeed"
+                : "selectedFeeling";
+        }
+    }
+
     return (
         <div className="container">
             <div className="row text-center text-lg-start">
-                {cards.map(c => {
-                    return <div className="card col-lg-3 col-md-4 col-sm-12">
+                {cards.map(card => {
+                    return <div
+                        className={"card col-lg-3 col-md-4 col-sm-12 " + selectedClass(card)}
+                        onClick={_ => props.onCardClick(card)}
+                        key={key(card)}
+                    >
                         <div className="card-body">
-                            {c.data.map(t => <p className={fontClass(t)}>{t.text}</p>)}
+                            {card.data.map(t => <p className={fontClass(t)}>{t.text}</p>)}
                         </div>
                     </div>
                 })}

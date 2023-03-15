e3b06e94dfa1050220358e732eeccbe2402afeb1
Lukas Krecan <lukas@krecan.net>
2023-03-15 22:05:08 +0100

    Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {CardData, CardInfo, feelings, findCard, needs} from './Data';
-import {Card, Container, Navbar, Row, Tab, Tabs} from "react-bootstrap";
+import {Card, Container, Nav, Navbar, NavbarBrand, Row, Tab, Tabs} from "react-bootstrap";
 import Dragula from "react-dragula";
 
 type NvcCardsAppProps = {};
@@ -41,39 +41,16 @@ class App extends React.Component<
     render() {
         return (
             <Container>
-                <Navbar bg="light" expand="lg" sticky="top">
-                    <a className="navbar-brand" href="#">NvcCards</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" href="#">Action</a>
-                                    <a className="dropdown-item" href="#">Another action</a>
-                                    <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
+                <Navbar bg="light" expand="sm" sticky="top">
+                    <NavbarBrand>NvcCards</NavbarBrand>
+                    <Navbar.Toggle aria-controls="navbarSupportedContent" />
+                    <Navbar.Collapse id="navbarSupportedContent">
+                        <Nav>
+                            <Nav.Link>Pocity</Nav.Link>
+                            <Nav.Link>Potřeby</Nav.Link>
+                            <Nav.Link>Výběr</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
                 <Tabs>
                     <Tab eventKey="needs" title="Potřeby" key="needs">

6293544a8599b5aa6b4b051cf5033b6ad2300354
Lukas Krecan <lukas@krecan.net>
2024-07-04 12:45:57 +0200

    Drop unused elements

--- src/SelectionMenu.tsx
+++ src/SelectionMenu.tsx
@@ -1,4 +1,4 @@
-import {Nav, Navbar, OverlayTrigger, Tooltip} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
